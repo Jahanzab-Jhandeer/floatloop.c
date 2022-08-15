19867235888a8239644e500a06b5bd3845606074
Jahanzab-Jhandeer <74298308+Jahanzab-Jhandeer@users.noreply.github.com>
2022-08-15 03:01:40 -0700

    Add files via upload

---
+++ floatloop.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main () {
+    // int i = 0.1;
+    // for(float i = 0.1; i <= 5.0; i = i= i+0.1) {
+    //     printf("%f\n" , i);
+    // }
+
+    for(char ch = 'a'; ch <= 'z'; ch ++) {
+        printf("%c\n" , ch);
+    }
+    return 0;
+}
